ac8642e95d42668bddd56db60b3007266b3975f6
Marinho.Rickson <44588839+RicksonM@users.noreply.github.com>
2020-07-02 12:17:36 -0300

    Revert "Correção do erro na cor do status de contrato"

--- pretoplus/src/main/resources/static/js/contratos.js
+++ pretoplus/src/main/resources/static/js/contratos.js
@@ -13,85 +13,85 @@ function fEditarPer() {
 
 
 // Abrir form contrato
-// let modal =  document.getElementsByClassName("modal")[0];
-// let fContrato =  document.getElementsByClassName("formContrato")[0];
+let modal =  document.getElementsByClassName("modal")[0];
+let fContrato =  document.getElementsByClassName("formContrato")[0];
 
 
-// function test() {
-// 	 window.location.href = "@{/exibir/{id}(id=${contratos.id})}";
+function test() {
+	 window.location.href = "@{/exibir/{id}(id=${contratos.id})}";
 	 
-// 	}
+	}
 
-// function abrirFormContrato() {
-//    modal.classList.add("modalAparece");
-//    fContrato.classList.add("formContratoAparece");
-// }
+function abrirFormContrato() {
+   modal.classList.add("modalAparece");
+   fContrato.classList.add("formContratoAparece");
+}
 
-// function sumirModal() {
-//     fContrato.classList.remove("formContratoAparece");
-//     modal.classList.remove("modalAparece");
-// }
+function sumirModal() {
+    fContrato.classList.remove("formContratoAparece");
+    modal.classList.remove("modalAparece");
+}
 
-// window.onclick = (event) => {
-//     if (event.target == modal) {
-//         fContrato.classList.remove("formContratoAparece");
-//         modal.classList.remove("modalAparece");
-//     }
-// }
+window.onclick = (event) => {
+    if (event.target == modal) {
+        fContrato.classList.remove("formContratoAparece");
+        modal.classList.remove("modalAparece");
+    }
+}
 
-// // Mudar cor inputs date
-// function MudarCorDaIni() {
-//     let dataIni = document.getElementById("dataIni");
-//     dataIni.style.color = "black";
-// }
+// Mudar cor inputs date
+function MudarCorDaIni() {
+    let dataIni = document.getElementById("dataIni");
+    dataIni.style.color = "black";
+}
 
-// function MudarCorDaTer() {
-//     let dataTer = document.getElementById("dataTer");
-//     dataTer.style.color = "black";
-// }
+function MudarCorDaTer() {
+    let dataTer = document.getElementById("dataTer");
+    dataTer.style.color = "black";
+}
 
-// // Upload de imagens
-// window.URL = window.URL || window.webkitURL;
+// Upload de imagens
+window.URL = window.URL || window.webkitURL;
 
-// var fileSelect = document.getElementById("fileSelect"),
-//     fileElem = document.getElementById("fileElem");
+var fileSelect = document.getElementById("fileSelect"),
+    fileElem = document.getElementById("fileElem");
 
-// fileSelect.addEventListener("click", function (e) {
-//   if (fileElem) {
-//     fileElem.click();
-//   }
-//   e.preventDefault(); // prevent navigation to "#"
-// }, false);
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault(); // prevent navigation to "#"
+}, false);
 
-// function handleFiles(files) {
-//   if (!files.length) {
-//     console.log("<p>No files selected!</p>");
-//   } else {
-//     var list = document.createElement("ul");
-//     list.setAttribute("class", "ulImg");
-//     for (var i = 0; i < files.length; i++) {
-//       var li = document.createElement("li");
-//       li.setAttribute("class", "liImg");
-//       list.appendChild(li);
+function handleFiles(files) {
+  if (!files.length) {
+    console.log("<p>No files selected!</p>");
+  } else {
+    var list = document.createElement("ul");
+    list.setAttribute("class", "ulImg");
+    for (var i = 0; i < files.length; i++) {
+      var li = document.createElement("li");
+      li.setAttribute("class", "liImg");
+      list.appendChild(li);
       
-//       var img = document.createElement("img");
-//       img.src = window.URL.createObjectURL(files[i]);
-//       img.setAttribute("class", "imgImg");
-//       // img.setAttribute("onclick", "verImg()");
-//       // img.height = 60;
-//       img.onload = function(e) {
-//         window.URL.revokeObjectURL(this.src);
-//       }
-//       li.appendChild(img);
+      var img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.setAttribute("class", "imgImg");
+      // img.setAttribute("onclick", "verImg()");
+      // img.height = 60;
+      img.onload = function(e) {
+        window.URL.revokeObjectURL(this.src);
+      }
+      li.appendChild(img);
       
-//       var info = document.createElement("span");
-//       info.setAttribute("class", "spanImg");
-//       info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
-//       li.appendChild(info);
-//     }
-//     fileList.appendChild(list);
-//   }
-// }
+      var info = document.createElement("span");
+      info.setAttribute("class", "spanImg");
+      info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+      li.appendChild(info);
+    }
+    fileList.appendChild(list);
+  }
+}
 
 
 //Status contratos
